cc26f11189546cd3a7a078ec0b2396a743c14692
Weact <ldruckes@orange.fr>
2022-10-22 11:28:17 +0200

    add bat and pause

--- Scenes/Character/Bat/Bat.gd
+++ Scenes/Character/Bat/Bat.gd
@@ -1,9 +1,16 @@
 extends Node2D
+class_name Bat
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	pass
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		move_bat(event.relative)
+
+func move_bat(pos):
+	position += pos

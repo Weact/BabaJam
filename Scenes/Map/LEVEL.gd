3abca2e0384c42020ec5175bfe1576e99420e6a3
JuuJHaaH <julien.hanns7gmail.com>
2022-10-22 11:17:04 +0200

    ajout d'une citrouille aléatoire dans le monde toute les 3s

--- Scenes/Map/LEVEL.gd
+++ Scenes/Map/LEVEL.gd
@@ -1,11 +1,13 @@
 extends Node2D
 onready var pumpkin_scene : PackedScene = preload("res://Scenes/Object/Pumpkin/Pumpkin.tscn")
-
+onready var pumpkin_timer : Timer = get_node("Timer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
+	pumpkin_timer.connect("timeout" , self , "_on_pumpkin_timer_timeout")
 	add_pumpkin()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	pass
@@ -17,3 +19,6 @@ func add_pumpkin() -> void :
 	var new_pumpkin_instance = pumpkin_scene.instance()
 	new_pumpkin_instance.set_position(coord)
 	call_deferred("add_child",new_pumpkin_instance)
+	
+func _on_pumpkin_timer_timeout() -> void :
+	add_pumpkin()

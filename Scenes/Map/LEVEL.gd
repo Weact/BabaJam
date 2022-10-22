bf25ed7a3311ba32b6d93951d794d20f978c0df5
JuuJHaaH <julien.hanns7gmail.com>
2022-10-22 12:16:56 +0200

    ajout de getters et de setters divers pour la classe Pumpkin

--- Scenes/Map/LEVEL.gd
+++ Scenes/Map/LEVEL.gd
@@ -1,12 +1,12 @@
 extends Node2D
 onready var pumpkin_scene : PackedScene = preload("res://Scenes/Object/Pumpkin/Pumpkin.tscn")
 onready var pumpkin_timer : Timer = get_node("Timer")
-
+var pumpkin_spawn_counter : int = 0
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
 	pumpkin_timer.connect("timeout" , self , "_on_pumpkin_timer_timeout")
-	add_pumpkin()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -19,6 +19,8 @@ func add_pumpkin() -> void :
 	var new_pumpkin_instance = pumpkin_scene.instance()
 	new_pumpkin_instance.set_position(coord)
 	call_deferred("add_child",new_pumpkin_instance)
+	pumpkin_spawn_counter +=1
 	
 func _on_pumpkin_timer_timeout() -> void :
 	add_pumpkin()
+	

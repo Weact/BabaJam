445ffaa2dffea5f60e2319cee9b4c230326954f7
Weact <ldruckes@orange.fr>
2022-10-22 20:36:40 +0200

    build version and bat counter

--- Global/GAME.gd
+++ Global/GAME.gd
@@ -1,7 +1,10 @@
 extends Node
 
+signal bat_added(total_bats)
+
 var models : int = 4
 var bat_model : String = "IdlePurple"
+var bat_icon_texture : Texture = null
 
 func _ready() -> void:
 	randomize()
@@ -13,11 +16,12 @@ func _input(event):
 		if event.is_action_pressed("fullscreen"):
 			OS.window_fullscreen = !OS.window_fullscreen
 		elif event.is_action_pressed("pause"):
-			get_tree().paused = !get_tree().paused
-			var new_mode = Input.MOUSE_MODE_VISIBLE \
-				if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED \
-				else Input.MOUSE_MODE_CAPTURED
-			set_new_mouse_mode(new_mode)
+			get_tree().quit()
+#			get_tree().paused = !get_tree().paused
+#			var new_mode = Input.MOUSE_MODE_VISIBLE \
+#				if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED \
+#				else Input.MOUSE_MODE_CAPTURED
+#			set_new_mouse_mode(new_mode)
 		elif event.is_action_pressed("restart"):
 			get_tree().reload_current_scene()
 			bat_model = get_bat_model()
@@ -31,12 +35,16 @@ func get_bat_model() -> String:
 	match(chosen_model_index):
 		0:
 			model = "IdlePurple"
+			bat_icon_texture = load("res://Assets/Bats/Bat face 1 violet.png")
 		1:
 			model = "IdleOrange"
+			bat_icon_texture = load("res://Assets/Bats/Bat face 1 orange.png")
 		2:
 			model = "IdleEvolutionPurple"
+			bat_icon_texture = load("res://Assets/Bats/Bat face 2 violet.png")
 		3:
 			model = "IdleEvolutionOrange"
+			bat_icon_texture = load("res://Assets/Bats/Bat face 2 orange.png")
 		_:
 			model = "IdlePurple"
 	return model
